d61f6d472ca906a27d9d26a2ff16660e504c7421
wangxingkang <wang_xingkang@qq.com>
2023-12-29 23:15:26 +0800

    chore: add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import './app.css'
 import App from './App.svelte'
+import './global.css'
 
 const app = new App({
   target: document.getElementById('app'),
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: [
+    './index.html',
+    './src/**/*.{svelte,js,ts}'
+  ], 
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
